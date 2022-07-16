d643077bcfbb228a2e4cd86a8ad9c426c4b71f3d
Stan <stan2dor@gmail.com>
2022-07-16 14:19:06 -0700

    fixed lint errors

--- components/common/atoms/Dropdown.js
+++ components/common/atoms/Dropdown.js
@@ -1,43 +1,40 @@
-import React, { Component } from 'react';
-import propTypes from 'prop-types';
+import React, { Component } from "react";
+import propTypes from "prop-types";
 
 class Dropdown extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      isOpen: false
+      isOpen: false,
     };
-
   }
   render() {
     const { isOpen } = this.state;
-    const { children, menu } = this.props;
+    // const { children, menu } = this.props;
+    const { children } = this.props;
 
     return (
-      <div
-        className="position-relative">
+      <div className='position-relative'>
         <div
-          className={`dropdown bg-white d-flex align-items-center justify-content-between ${isOpen &&
-            'border-color-black'}`}
-        >
-          <p className="font-color-medium">{this.props.value || this.props.placeholder }</p>
-          <img
-            src="/icon/arrow-bottom.svg"
-            className="w-20"
-          />
+          className={`dropdown bg-white d-flex align-items-center justify-content-between ${
+            isOpen && "border-color-black"
+          }`}>
+          <p className='font-color-medium'>
+            {this.props.value || this.props.placeholder}
+          </p>
+          <img src='/icon/arrow-bottom.svg' className='w-20' alt='arrow' />
           <select
             name={this.props.name}
             disabled={this.props.disabled}
             value={this.props.value}
-            className="position-absolute top-0 right-0 bottom-0 left-0 opacity-0 pointer w-100">
-            <option value="" disabled>
-              { this.props.disabledOptionText || 'Select an option' }
+            className='position-absolute top-0 right-0 bottom-0 left-0 opacity-0 pointer w-100'>
+            <option value='' disabled>
+              {this.props.disabledOptionText || "Select an option"}
             </option>
             {children}
           </select>
         </div>
-
       </div>
     );
   }
@@ -48,5 +45,5 @@ Dropdown.propTypes = {
   placeholder: propTypes.string,
   disabled: propTypes.string,
   disabledOptionText: propTypes.string,
-}
+};
 export default Dropdown;
